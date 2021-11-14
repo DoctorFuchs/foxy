e5a2cbc5ae55c87bf87124bc7e1abfe62af5b4ea
DoctorFuchs <fuchs9203@gmail.com>
2021-11-14 14:43:41 +0100

    example tip

--- examples/createtable.py
+++ examples/createtable.py
@@ -4,3 +4,6 @@ yourDatabase = database.database("your_database_name")
 yourTable = yourDatabase.createTable("A database name")
 
 # now use your table for whatever
+
+# don't forget to save the database
+yourDatabase.save()
